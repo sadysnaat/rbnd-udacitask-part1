7b114996fc446b5d2efafe2e057413322610ca8e
Deepak Sharma <sadysnaat@gmail.com>
2016-02-11 01:04:21 +0530

    Implemented Basic Todo List

--- todolist.rb
+++ todolist.rb
@@ -1,7 +1,22 @@
 class TodoList
+
+    attr_reader :title, :items
     # methods and stuff go here
+    def initialize(list_title)
+      @title = list_title
+      @items = Array.new
+    end
+
+    def add_item(new_item)
+      item = Item.new(new_item)
+      @items.push(item)
+    end
 end
 
 class Item
     # methods and stuff go here
+    def initialize(description)
+      @description = description
+      @completion_status = false
+    end
 end
