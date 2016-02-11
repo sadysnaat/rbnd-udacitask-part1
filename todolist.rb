0a3bd559fa0f6f7e13f62b033fcdaa3e10c37ada
Deepak Sharma <sadysnaat@gmail.com>
2016-02-11 13:20:02 +0530

    Completed udacitask instructions

--- todolist.rb
+++ todolist.rb
@@ -1,5 +1,4 @@
 class TodoList
-
     attr_reader :title, :items
     # methods and stuff go here
     def initialize(list_title)
@@ -7,16 +6,46 @@ class TodoList
       @items = Array.new
     end
 
+    def rename_list(new_name)
+      @title = new_name
+    end
+
     def add_item(new_item)
       item = Item.new(new_item)
       @items.push(item)
     end
+
+    def delete_item(item_no)
+      @items.delete_at(item_no-1)
+    end
+
+    def toggle_status(item_no)
+      @items[item_no-1].toggle!
+    end
+
+    def print_list()
+      puts "*"*10
+      puts @title
+      puts "*"*10
+
+      sorted_array = @items.sort_by { |item| item.description }
+      sorted_array.each_index { |index|  puts "#{index+1} #{@items[index].description} #{@items[index].completed?}"  }
+    end
 end
 
 class Item
+    attr_reader :description
     # methods and stuff go here
     def initialize(description)
       @description = description
       @completion_status = false
+    end
+
+    def toggle!
+      @completion_status = !@completion_status
+    end
+
+    def completed?
+      @completion_status
     end
 end
--- udacitask.rb
+++ udacitask.rb
@@ -1,23 +1,37 @@
 require_relative 'todolist.rb'
 
 # Creates a new todo list
+list = TodoList.new("Buy Grocery")
 
 # Add four new items
+list.add_item("Milk")
+list.add_item("Sugar")
+list.add_item("Tea")
+list.add_item("Coffee")
 
 # Print the list
+list.print_list()
 
 # Delete the first item
+list.delete_item(1)
 
 # Print the list
+list.print_list()
 
 # Delete the second item
+list.delete_item(2)
 
 # Print the list
+list.print_list()
 
 # Update the completion status of the first item to complete
+list.toggle_status(1)
 
 # Print the list
+list.print_list()
 
 # Update the title of the list
+list.rename_list("Deepak's Grocery List")
 
 # Print the list
+list.print_list()
